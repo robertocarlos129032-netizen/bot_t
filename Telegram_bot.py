e03532b177bb7ef903b2bb0efc2f91206780483e
robertocarlos129032-netizen <robertocarlos129032@gmail.com>
2026-03-15 21:15:03 -0400

    27

--- Telegram_bot.py
+++ Telegram_bot.py
@@ -281,7 +281,7 @@ async def send_links(update: Update, context: ContextTypes.DEFAULT_TYPE):
             f"🔗 Aquí tienes los links:\n\n{links_text}"
         )
 def run_bot():
-    TOKEN = os.getenv("8613878245:AAGfTjdnjDJOUtAbqVJkiXZZweguYc_ykyw")
+    TOKEN = os.getenv("BOT_TOKEN")
 
     app = ApplicationBuilder().token(TOKEN).build()
 
